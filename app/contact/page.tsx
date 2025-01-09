7c1cc322873bb062c33437384494cc4e4670b9ab
Alejandra Guadalupe L. Altamirano <pandita420@MacBook-Pro-de-Alejandra.local>
2025-01-09 17:56:48 -0500

    changing data

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/aleehpandita420",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@aleehpandita420",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:alejandra.altamirano19@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "alejandra.altamirano19@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/aleehpandita",
 		label: "Github",
-		handle: "chronark",
+		handle: "aleehpandita",
 	},
 ];
 
